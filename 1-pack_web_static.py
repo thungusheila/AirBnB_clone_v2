090f77f6332f4ff97677df3ef3dd4da311269604
karivone <karimiyvonne19@gmail.com>
2023-04-05 23:05:42 -0700

    configure web static

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -9,10 +9,14 @@ from os.path import isdir
 def do_pack():
     """generates a tgz archive"""
     try:
-        date = datetime.now().strftime("%Y%m%d%H%M%S")
+        #uses module date time to form at the name of the compressed file
+        date = datetime.utcnow().strftime("%Y%m%d%H%M%S")
+        #creates a folder if it doesn't already exist
         if isdir("versions") is False:
-            local("mkdir versions")
+            local("mkdir -p versions")
+        #define the format in which the compressed file is saved
         file_name = "versions/web_static_{}.tgz".format(date)
+        #defines command to compress coontents
         local("tar -cvzf {} web_static".format(file_name))
         return file_name
     except:
